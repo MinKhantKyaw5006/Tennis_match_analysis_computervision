7a318982428b27a2f30575bbe5a5438e68fcac0b
MinKhantKyaw <minkhant050603@gmail.com>
2025-04-04 14:06:51 +0700

    test with the new best and last yolo model

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('models/best.pt')
 
-result = model.predict('input_videos/input_video.mp4',save= True)
+result = model.predict('input_videos/input_video.mp4',conf = 0.2 ,save= True)
 print(result)
 print("boxes: ")
 for box in result[0].boxes:
     print(box)
+
+
+#learn roboflow
